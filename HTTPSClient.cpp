174f880aab14dab390630df988ad01418fccc6f2
Stephen Oliver <steve@infincia.com>
2019-06-06 00:07:12 -0400

    Remove unused code

--- HTTPSClient.cpp
+++ HTTPSClient.cpp
@@ -40,10 +40,6 @@ static esp_err_t _http_event_handle(esp_http_client_event_t *evt) {
             break;
         case HTTP_EVENT_ON_DATA:
             // ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-            // if (!esp_http_client_is_chunked_response(evt->client)) {
-            //     printf("%.*s", evt->data_len, (char *)evt->data);
-            // }
-
             break;
         case HTTP_EVENT_ON_FINISH:
             // ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
