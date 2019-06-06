a4019dad1a18abbcea25c867e3830fc1099caa5b
Stephen Oliver <steve@infincia.com>
2019-06-06 00:07:53 -0400

    Move log statements to verbose level and uncomment

--- HTTPSClient.cpp
+++ HTTPSClient.cpp
@@ -26,26 +26,26 @@ static esp_err_t _http_event_handle(esp_http_client_event_t *evt) {
 
     switch (evt->event_id) {
         case HTTP_EVENT_ERROR:
-            // ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+            ESP_LOGV(TAG, "HTTP_EVENT_ERROR");
             break;
         case HTTP_EVENT_ON_CONNECTED:
-            // ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+            ESP_LOGV(TAG, "HTTP_EVENT_ON_CONNECTED");
             break;
         case HTTP_EVENT_HEADER_SENT:
-            // ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+            ESP_LOGV(TAG, "HTTP_EVENT_HEADER_SENT");
             break;
         case HTTP_EVENT_ON_HEADER:
-            // ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER");
+            ESP_LOGV(TAG, "HTTP_EVENT_ON_HEADER");
             // printf("%.*s", evt->data_len, (char *)evt->data);
             break;
         case HTTP_EVENT_ON_DATA:
-            // ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            ESP_LOGV(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
             break;
         case HTTP_EVENT_ON_FINISH:
-            // ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+            ESP_LOGV(TAG, "HTTP_EVENT_ON_FINISH");
             break;
         case HTTP_EVENT_DISCONNECTED:
-            // ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            ESP_LOGV(TAG, "HTTP_EVENT_DISCONNECTED");
             break;
     }
     return ESP_OK;
